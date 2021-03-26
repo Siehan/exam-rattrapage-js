2e14f4fb35ce54bbeac854ed0a53ec74f0a6df13
Siehan <memain.ye.sylvie@gmail.com>
2021-03-26 17:19:30 +0100

    pas done

---
+++ fiveWayLoop.js
@@ -0,0 +1,6 @@
+/*
+fiveWayLoop.js
+
+Même exercice que précédemment mais ajouter deux boucles do-while et while pour effectuer le traitement.
+
+*/
---
+++ isEven.js
@@ -0,0 +1,5 @@
+/*
+isEven.js
+Ecrire une fonction isEven qui prend un nombre en paramètre et qui devra retourner true
+si le nombre passé en paramètre est pair, sinon la fonction retournera false.
+*/
