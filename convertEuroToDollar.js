170d4416a484caa261829ed29ea93a98657f6452
Siehan <memain.ye.sylvie@gmail.com>
2021-03-26 17:19:12 +0100

    a moitié done

---
+++ convertEuroToDollar.js
@@ -0,0 +1,11 @@
+/*
+convertEuroToDollar.js
+Ecrire une fonction convertEuroToDollar qui fait la conversion
+d'une somme exprimée en Euros, en Dollars. 1 euro est égal à 1.18 dollars
+*/
+
+function convertEuroToDollar() {
+  eur.value = parseFloat(usd.value) * 1.18;
+}
+
+console.log(convertEuroToDollar + " dollar");
